3d7479cab6b968005b73d7f84c94980a0241abef
Coeat.k <coeat.k@kakaocorp.com>
2022-08-22 01:35:23 +0900

    feat:init

---
+++ src/test/java/com/rxjava/rxjavaexample/RxJavaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxjava.rxjavaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxJavaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
